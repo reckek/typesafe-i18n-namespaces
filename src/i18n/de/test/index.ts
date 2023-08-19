bbfa65a3e1f3d2edf6f0e33d0a02b0d9b2f512eb
Alian <iskra23231@gmail.com>
2023-08-20 00:37:27 +0300

    init repo

---
+++ src/i18n/de/test/index.ts
@@ -0,0 +1,8 @@
+import type { NamespaceTestTranslation } from '../../i18n-types.js'
+
+const de_test = {
+	// TODO: insert translations
+
+} satisfies NamespaceTestTranslation
+
+export default de_test
---
+++ src/i18n/en/test/index.ts
@@ -0,0 +1,5 @@
+import { BaseTranslation } from "../../i18n-types";
+
+const en_test = {} satisfies BaseTranslation;
+
+export default en_test;
